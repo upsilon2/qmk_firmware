bd0814df5ce62d8cb6b774284327b12d9e4e0837
taise <taise515@gmail.com>
2016-10-18 13:06:19 +0900

    Change: Enter/Space/Backspace/Del position

--- keyboards/ergodox/ez/keymaps/taise/keymap.c
+++ keyboards/ergodox/ez/keymaps/taise/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                        | L2   | L1   |       | Alt  | Ctrl   ]
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | Home |       | PgUp |        |      |
- *                                 |Backsp| Del  |------|       |------|  Enter | Spc  |
+ *                                 |Space |Backsp|------|       |------| Del    | Enter|
  *                                 |      |      | End  |       | PgDn |        |      |
  *                                 `--------------------'       `----------------------'
  */
@@ -37,10 +37,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,     KC_Q,     KC_W,    KC_E,    KC_R,    KC_T, KC_LBRC,
         KC_LCTL,    KC_A,     KC_S,    KC_D,    KC_F,    KC_G,
         KC_LSFT,    KC_Z,     KC_X,    KC_C,    KC_V,    KC_B, KC_LANG2,
-        KC_CAPS, KC_LALT,  KC_LGUI, KC_LEFT, KC_RGHT,
+        KC_CAPS,    KC_LALT,  KC_LGUI, KC_LEFT, KC_RGHT,
                                                          KC_FN2,  KC_FN1,
                                                                   KC_HOME,
-                                                KC_BSPC, KC_DEL,  KC_END,
+                                                KC_SPC,  KC_BSPC, KC_END,
         // right hand
         KC_QUOT,    KC_7,     KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,
         KC_RBRC,    KC_Y,     KC_U,    KC_I,    KC_O,    KC_P, KC_BSLS,
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                               KC_DOWN, KC_UP,   KC_RCTL, KC_RGUI, KC_RALT,
         KC_RALT,    KC_RCTL,
         KC_PGUP,
-        KC_PGDN,    KC_ENT,   KC_SPC
+        KC_PGDN,    KC_DEL,   KC_ENT
     ),
 /* Keymap 1: Symbol Layer
  *
