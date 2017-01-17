58dcb3635275c8d329e2e35c882b2abea526a94e
taise <taise515@gmail.com>
2017-01-17 20:57:41 +0900

    Modified: '*' to '`'

--- keyboards/ergodox/ez/keymaps/taise/keymap.c
+++ keyboards/ergodox/ez/keymaps/taise/keymap.c
@@ -11,7 +11,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * | Esc    |   1  |   2  |   3  |   4  |   5  |   6  |           |   '  |   7  |   8  |   9  |   0  |   -  |   =    |
+ * | Esc    |   1  |   2  |   3  |   4  |   5  |   6  |           |   `  |   7  |   8  |   9  |   0  |   -  |   =    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |   [  |           |   ]  |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                   KC_HOME,
                                                 KC_SPC,  KC_BSPC, KC_END,
         // right hand
-        KC_QUOT,    KC_7,     KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,
+        KC_GRV,     KC_7,     KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,
         KC_RBRC,    KC_Y,     KC_U,    KC_I,    KC_O,    KC_P, KC_BSLS,
                     KC_H,     KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LANG1,   KC_N,     KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
